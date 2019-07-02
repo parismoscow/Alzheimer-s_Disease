b6129b00988cd4d45ba0fb6c09974c85fc5494e5
Jasmina Jovanovic <jasminajancic@gmail.com>
2019-07-02 17:03:14 -0500

    adding feature selection to models webpage

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
+
 
 app = Flask(__name__)
 
@@ -7,21 +8,35 @@ app = Flask(__name__)
 def diseases():
     return render_template("index.html")
 
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        print(result)
+        return render_template("result.html", result=result)
+    # else:
+
+
 @app.route("/data")
 def data():
     return render_template("data.html")
+
 
 @app.route("/resources")
 def resources():
     return render_template("resources.html")
 
+
 @app.route("/treatment")
 def treatment():
     return render_template("treatment.html")
 
+
 @app.route("/models")
 def models():
     return render_template("models.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
